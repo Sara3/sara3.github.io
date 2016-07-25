b6f8479dbf6264b4d1c21a2649f1366224eb3792
Sara <Sabera.daqiq@yahoo.com>
2016-07-24 23:38:38 -0700

    stuff

---
+++ assets/js/custom-fss.js
@@ -0,0 +1,18 @@
+/**
+ * ==============================
+ * Constellation Initialize Code
+ * ==============================
+ */
+// Init plugin
+if( $('#poligon-shape').hasClass('dark') ) {
+    var poligonContainer    = 'poligon-shape-outer-area',
+    poligonShape        = 'poligon-shape',
+    ambient_value       ="#2A2A2A",
+    diffuse_value       ="#222222";
+}
+else {
+    var poligonContainer    = 'poligon-shape-outer-area',
+    poligonShape        = 'poligon-shape',
+    ambient_value       ="#2A80B9",
+    diffuse_value       ="#F8626D";
+}
